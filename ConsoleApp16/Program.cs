2f532343ea7e7b203e9b18e9cd886108d2e9e60d
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-01-19 22:09:29 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp16;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+string deploymentName = config["OpenAI:DeploymentName"] ?? throw new InvalidOperationException("OpenAI:DeploymentName is not set.");
+string modelId = config["OpenAI:ModelId"] ?? throw new InvalidOperationException("OpenAI:ModelId is not set.");
+string endpoint = config["OpenAI:Endpoint"] ?? throw new InvalidOperationException("OpenAI:BaseUrl is not set.");
+string key = config["OpenAI:Key"] ?? throw new InvalidOperationException("OpenAI:Key is not set.");
+
+Kernel kernel = Kernel.CreateBuilder()
+ .AddAzureOpenAIChatCompletion(
+    deploymentName,
+    endpoint,
+    key).Build();
+
+kernel.Plugins.AddFromType<WeatherPlugin>();
+
+OpenAIPromptExecutionSettings? setting = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+
+};
+
+while (true)
+{
+    Console.Write("User > ");
+    string input = Console.ReadLine()!;
+    if (input == "exit")
+    {
+        break;
+    }
+    else
+    {
+        var result = await kernel.InvokePromptAsync(input, new(setting));
+        Console.WriteLine($"Assistant > {result}");
+    }
+}
---
+++ ConsoleApp16/WeatherPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace ConsoleApp16;
+
+public class WeatherPlugin
+{
+    private readonly HttpClient _client = new();
+
+    [KernelFunction, Description("天気を取得する場所コードを取得します。")]
+    public static int GetPlaceId([Description("天気を取得する場所")] string place)
+    {
+        var res = place switch
+        {
+            "東京" => 130000,
+            "横浜" => 140000,
+            "名古屋" => 230000,
+            "京都" => 260000,
+            "静岡" => 220000,
+            "福井" => 180000,
+            "新潟" => 150000,
+            "富山" => 160000,
+            "金沢" => 170000,
+            "岐阜" => 210000,
+            "長野" => 200000,
+            "高山" => 190000,
+            "松本" => 200000,
+            "大津" => 250000,
+            "大阪" => 270000,
+            "札幌" => 016000,
+            "仙台" => 040000,
+            "福岡" => 400000,
+            "那覇" => 471000,
+            _ => throw new ArgumentException("対応していない地域です。")
+        };
+        return res;
+    }
+
+    [KernelFunction, Description("場所コードの地域の天気を返す")]
+    public async Task<string> Weather([Description("場所コード")]int place)
+    {
+        return (await _client.GetAsync($"https://www.jma.go.jp/bosai/forecast/data/forecast/{place}.json")).Content.ReadAsStringAsync().Result;
+    }
+
+}
